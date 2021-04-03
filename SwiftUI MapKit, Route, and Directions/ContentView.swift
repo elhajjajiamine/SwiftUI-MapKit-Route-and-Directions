e20fc548ad2d959a2107b30be0ee178fbbdb1b61
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-03 15:31:36 +0100

    Initial Commit

---
+++ SwiftUI MapKit, Route, and Directions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI MapKit, Route, and Directions
+//
+//  Created by elhajjaji on 3/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
